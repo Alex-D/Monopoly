c81143b19b7adc3b374ed45db38545f6fea9b6ef
Alex-D <demodealex@gmail.com>
2013-12-09 17:20:22 +0100

    Typo

--- sources/monopoly/proprietes/Groupe.java
+++ sources/monopoly/proprietes/Groupe.java
@@ -1,18 +1,35 @@
-package monopoly.proprietes ;
+package monopoly.proprietes;
 
-import java.util.List ;
+import java.util.List;
 
-/** Cette interface décrit les fonctionnalités associées à un groupe
- * de propriétés */
-public interface Groupe {
-    /** L'intitulé du groupe */
-    String nom() ;
-    /** Le prix des constructions pour ce groupe */
-    int coutImmo() ;
-    /** La liste des propriétés qui le composent */
-    List<Propriete> composition() ;
-    /** Retourne le groupe dont le nom est spécifié */
-    Groupe get(String nom) ;
-    /** Indique si le groupe appartient entièrement à un seul joueur */
-    boolean proprietaireUnique() ;
+/**
+ * Cette interface décrit les fonctionnalités associées à un groupe
+ * de propriétés
+ */
+public interface Groupe
+{
+    /**
+     * L'intitulé du groupe
+     */
+    String nom();
+    
+    /**
+     * Le prix des constructions pour ce groupe
+     */
+    int coutImmo();
+    
+    /**
+     * La liste des propriétés qui le composent
+     */
+    List<Propriete> composition();
+    
+    /**
+     * Retourne le groupe dont le nom est spécifié
+     */
+    Groupe get(String nom);
+    
+    /**
+     * Indique si le groupe appartient entièrement à un seul joueur
+     */
+    boolean proprietaireUnique();
 }
