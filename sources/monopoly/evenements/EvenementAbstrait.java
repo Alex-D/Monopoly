a21310a58b813604f74d02714d7cf6c5a9262860
Alex-D <demodealex@gmail.com>
2013-12-10 14:45:14 +0100

    Ajout evenement abstrait

---
+++ sources/monopoly/evenements/EvenementAbstrait.java
@@ -0,0 +1,46 @@
+package monopoly.evenements;
+
+import monopoly.jeu.Joueur;
+import monopoly.jeu.Case;
+
+
+
+/**
+ * Cette interface décrit les fonctionnalités associées aux
+ * événements du jeu
+ */
+public abstract class EvenementAbstrait implements Evenement
+{
+    private String nom;		// Nom de l'évènement
+    private Joueur cible;	// Cible (joueur) qui subit l'évènement
+
+
+
+    /**
+     * Construit un évènement nommé et ciblé
+     */
+    public EvenementAbstrait(String nom)
+    {
+    	this.nom = nom;
+    }
+
+
+
+    public String nom()
+    {
+    	return nom;
+    }
+
+    public Joueur cible()
+    {
+    	return cible;
+    }
+
+    /**
+     * A surcharcher dans les enfants
+     */
+    public void declencher(Joueur joueur)
+    {
+    	this.joueur = joueur;
+    }
+}
