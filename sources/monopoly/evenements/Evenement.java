0da2672f94a9a468341f75e9016019751439acaf
Alex-D <demodealex@gmail.com>
2013-12-10 14:43:58 +0100

    Implémentation abstraite d'Evenement

--- sources/monopoly/evenements/Evenement.java
+++ sources/monopoly/evenements/Evenement.java
@@ -1,14 +1,29 @@
-package monopoly.evenements ;
+package monopoly.evenements;
 
-import monopoly.jeu.Joueur ;
-import monopoly.jeu.Case ;
+import monopoly.jeu.Joueur;
+import monopoly.jeu.Case;
 
-/** Cette interface décrit les fonctionnalités associées aux
- * événements du jeu */
-public interface Evenement {
-    /** Intitulé de l'événement (i.e. en principe de la case associée
-     * ou de la carte) */
-    String nom() ;
-    /** Le joueur qui subit l'événement */
-    Joueur cible() ;    
+
+
+/**
+ * Cette interface décrit les fonctionnalités associées aux
+ * événements du jeu
+ */
+public interface Evenement
+{
+    /**
+     * Intitulé de l'événement (i.e. en principe de la case associée
+     * ou de la carte)
+     */
+    String nom();
+
+    /**
+     * Le joueur qui subit l'événement
+     */
+    Joueur cible();
+
+    /**
+     * Déclenche l'évènement
+     */
+    void declencher();
 }
